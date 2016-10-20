9892837989286cbecb71909ab7b8bb3250aec2dd
gatogoku <aqv1793@gmail.com>
2016-10-20 11:53:40 +0200

    action bar funciona

--- app/src/main/java/android/frontend/classapp/org/classapp/MainActivity.java
+++ app/src/main/java/android/frontend/classapp/org/classapp/MainActivity.java
@@ -1,8 +1,13 @@
 package android.frontend.classapp.org.classapp;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -15,15 +20,28 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView listViewTask;
     private ArrayList<Classes> clases;
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //super.onCreate(savedInstanceState);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-
+       // setContentView(R.layout.activity_main);
         setContentView(R.layout.list_menu);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("MainActivity");
+
+
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
        // setTitle("CLASES");
         clases = new ArrayList<Classes>();
@@ -52,7 +70,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_star:
+                Log.d("PELLODEBUG","Star selected");
+                return true;
+            case R.id.action_settings:
+                Log.d("PELLODEBUG","Settings selected");
+                return true;
+          /*  case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;*/
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
 
 
